7c1e7674cd0b43cd5bf7c3acf012113960bd10bc
esneyder_a@hotmail.com <maxtermax>
2015-06-19 16:31:23 -0500

    add readme

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,31 +2,14 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
-var _ = require('underscore');
 var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
-
-    // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the priceless ' + chalk.red('N') + ' generator!'
     ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
-
-      done();
-    }.bind(this));
   },
 
   writing: {
