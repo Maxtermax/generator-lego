a5ecc54e072fe0d5f1f0b5932f12258edcc1f981
esneyder_a@hotmail.com <maxtermax>
2015-06-21 00:25:36 -0500

    files

--- test/setting/model/schemas/index.js
+++ test/setting/model/schemas/index.js
@@ -1,7 +1,8 @@
 module.exports = function (Schema) {
 		//begin ListSchemas
 		var Dog = new Schema({ 
-			name:{type:String,required:true,unique:false}//last field
+			name:{type:String,required:true,unique:false},
+			age:{ type:Number,required:true,unique:false}//last field
 		})//end Dog
 		
 		var Cat = new Schema({
